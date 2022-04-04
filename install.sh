c82ff26ea9c34447a77584055da9fa4fd8dd0cbb
Amber Cronin <amber@amber.vision>
2022-04-04 11:07:49 +0100

    Update install script with working data

--- install.sh
+++ install.sh
@@ -24,13 +24,13 @@ cp .env.template .env
 sed -i '23,48d' .env
 sed -i '17,18d' .env
 sed -i 's/your-target-account-role-name (e.g. ccf-app)/ccf-api-role/g' .env
-sed -i 's/your-athena-db-name/ccf_athena/g' .env
-sed -i 's/your-athena-db-table/billing/g' .env
+sed -i 's/your-athena-db-name/ccf/g' .env
+sed -i 's/your-athena-db-table/ccf/g' .env
 sed -i 's/your-athena-region/eu-west-2/g' .env
 sed -i 's/your-athena-query-results-location/internal-cloud-carbon-footprint-athena-results/g' .env
 sed -i 's/your-billing-account-id/445220836204/g' .env
 sed -i 's/your-billing-account-name/transform/g' .env
-sed -i 's/=default/=IAM/g' .env
+sed -i 's/=default/=AWS/g' .env
 
 cd ../..
 
@@ -42,7 +42,6 @@ sed -i 's/REACT_APP_PREVIOUS_YEAR_OF_USAGE/#REACT_APP_PREVIOUS_YEAR_OF_USAGE/g' 
 sed -i 's/=2/=12/g' .env
 
 cat <<EOF >> .env
-HOST=localhost
 PORT=80
 EOF
 
