83266420c9cc8390987fdf5139ed4d9e2ba9d23f
Amber Cronin <amber@amber.vision>
2022-06-14 12:15:42 +0100

    s/name/bucket/

--- locals.tf
+++ locals.tf
@@ -10,7 +10,7 @@ locals {
     "AWS_ATHENA_DB_NAME"               = aws_athena_database.main.name
     "AWS_ATHENA_DB_TABLE"              = "billing_data"
     "AWS_ATHENA_REGION"                = var.aws_region
-    "AWS_ATHENA_QUERY_RESULT_LOCATION" = "s3://${aws_s3_bucket.athena.name}"
+    "AWS_ATHENA_QUERY_RESULT_LOCATION" = "s3://${aws_s3_bucket.athena.bucket}"
     "AWS_BILLING_ACCOUNT_ID"           = data.aws_caller_identity.current.account_id
     "AWS_BILLING_ACCOUNT_NAME"         = data.aws_iam_account_alias.current.account_alias
     "AWS_AUTH_MODE"                    = "AWS"
