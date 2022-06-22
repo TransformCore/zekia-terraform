d0092d47c85d45c7534073459378adf395bbd79b
Amber Cronin <amber@amber.vision>
2022-06-22 09:13:54 +0100

    Refactor ECS task role policy

--- iam.tf
+++ iam.tf
@@ -3,23 +3,19 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "ecs_task_execution_role" {
   version = "2012-10-17"
   statement {
-    sid     = ""
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
+    sid    = ""
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+      "athena:*",
+      "ce:GetRightsizingRecommendation",
+      "glue:*"
+    ]
 
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "athena:*",
-      "ce:GetRightsizingRecommendation",
-      "glue:*"
-    ]
   }
 }
 
