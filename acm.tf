3a21e1f0f6ae1d90a6d343932b9df6d25f96acbd
Amber Cronin <amber@amber.vision>
2022-06-23 15:10:43 +0100

    Update wildcard cert validation

--- acm.tf
+++ acm.tf
@@ -28,7 +28,6 @@ resource "aws_acm_certificate_validation" "root" {
 }
 
 resource "aws_acm_certificate_validation" "wildcard" {
-  provider                = aws.cloudfront
   certificate_arn         = aws_acm_certificate.wildcard.arn
   validation_record_fqdns = [for record in aws_route53_record.wildcard_validation : record.fqdn]
 
