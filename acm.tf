716908b53b086565b94e2c87d7498b769ee47dd8
Chris Mills <7100370+chriscoffee@users.noreply.github.com>
2022-06-23 13:03:22 +0100

    We should be using the wildcard everywhere
    - We should be using the wildcard not the non wildcard certs in places,
      that way we can do fun stuff.
    - We should use the correct health check now

--- acm.tf
+++ acm.tf
@@ -1,22 +1,3 @@
-resource "aws_acm_certificate" "lb" {
-  domain_name       = local.domain
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate" "root" {
-  provider          = aws.cloudfront
-  domain_name       = local.domain
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_acm_certificate" "wildcard" {
   provider          = aws.cloudfront
   domain_name       = "*.${local.domain}"
@@ -31,25 +12,6 @@ resource "aws_acm_certificate_validation" "wildcard" {
   provider                = aws.cloudfront
   certificate_arn         = aws_acm_certificate.wildcard.arn
   validation_record_fqdns = [for record in aws_route53_record.wildcard_validation : record.fqdn]
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_acm_certificate_validation" "root" {
-  provider                = aws.cloudfront
-  certificate_arn         = aws_acm_certificate.root.arn
-  validation_record_fqdns = [for record in aws_route53_record.root_validation : record.fqdn]
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_acm_certificate_validation" "lb" {
-  certificate_arn         = aws_acm_certificate.lb.arn
-  validation_record_fqdns = [for record in aws_route53_record.lb_validation : record.fqdn]
 
   timeouts {
     create = "5m"
